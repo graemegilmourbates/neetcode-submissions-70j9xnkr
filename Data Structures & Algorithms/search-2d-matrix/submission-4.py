bd4c8a9b624712e74b0934623b6e385b66b98a6b
Graeme Gilmour Bates <65102258+graemegilmourbates@users.noreply.github.com>
2026-04-08 20:20:40 -0700

    Add: search-2d-matrix - submission-4

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-4.py
@@ -0,0 +1,15 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        n = len(matrix)
+        m = len(matrix[0])
+        i=0;
+        while(target>matrix[i][m-1]):
+            i += 1
+            if(i>=n):
+                return False
+        j=0;
+        while(j<m):
+            if(matrix[i][j]==target):
+                return True
+            j+=1
+        return False
